bc1db0f51af0e82f6c01cf1e2989f9c2bab97971
janci <jancotomas@gmail.com>
2016-01-03 14:06:17 +0100

    fixed last game time comparator

--- src/RmngCurveFever.java
+++ src/RmngCurveFever.java
@@ -33,7 +33,7 @@ import javax.swing.JOptionPane;
 public class RmngCurveFever {
     
     // number of seconds between forum checks
-    private final static int checkInterval = 30; 
+    private final static int checkInterval = 10; 
     private final String forumUrl = "http://www.rouming.cz/roumingForum.php";
     private final Pattern feverRegex = Pattern.compile("\"(.*curvefever\\.com[^\"]*)");
     private final Pattern dateRegex = Pattern.compile("\\(([0-9]{1,2})\\.([0-9]{1,2}).([0-9]{4}) ([0-9]{1,2}):([0-9]{1,2})\\)");
@@ -126,7 +126,7 @@ public class RmngCurveFever {
 	if(lastGame != null){
 	    lastDate = lastGame.date;
 	}
-	return g.date.after(lastDate);
+	return g.date.getTime() > (lastDate.getTime() + 10000);
     }
     
     
